e7e6badc8b53cc41dd8aa8d2315af01f92e77d3f
Jananya K H <137055462+jananyajanu@users.noreply.github.com>
2024-07-28 16:33:34 +0530

    Routes and Hoc

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,25 +1,37 @@
 import "./App.css";
-import ClassComponent from "./components/ClassComponent";
-import FunctionComponent from "./components/FunctionComponent";
+import ClassComponent from "./pages/ClassComponent";
+import FunctionComponent from "./pages/FunctionComponent";
 import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import BaseHoc from "./hoc/BaseHoc";
 
 function App() {
   const author = "rk";
   const [name, setName] = useState("DI");
   return (
-    <div className="App">
-      <p>Class Component</p>
-      <ClassComponent />
-
-      <p>Function Component</p>
-      {/* <FunctionComponent name={"Devtown"} age={20} author={author} /> */}
-      <FunctionComponent
-        name={name}
-        age={20}
-        author={author}
-        setName={setName}
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <BaseHoc>
+            <ClassComponent />
+          </BaseHoc>
+        }
       />
-    </div>
+      <Route
+        path="/functional-component"
+        element={
+          <BaseHoc>
+            <FunctionComponent
+              name={name}
+              age={10}
+              author={author}
+              setName={setName}
+            />
+          </BaseHoc>
+        }
+      />
+    </Routes>
   );
 }
 
